26a6b708956e72fa64478ecac01650efb78a48b0
daramosn <daramosn@unal.edu.co>
2024-06-11 12:16:39 -0500

    feat: add creation of new reminders and displaying on calendar

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <header>
+      <h1>Calendar app</h1>
+    </header>
+    <main>
+      <MyMonth :year="2024" :month="2" />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MyMonth from './components/MyMonth.vue'
+</script>
+
+<style>
+.layout {
+  padding: 32px max(32px, calc((100% - var(--body)) / 2));
+  min-height: 100svh;
+  height: 100svh;
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
+</style>
---
+++ src/components/MyMonth.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="month">
+    <DayItem v-for="date in monthArray" :key="date" :month="month" :year="year" :date="date" />
+  </section>
+  <ReminderForm />
+</template>
+
+<script lang="ts" setup>
+import DayItem from './DayItem.vue'
+import ReminderForm from './ReminderForm.vue'
+interface Props {
+  year: number
+  month: number
+}
+
+const { month, year } = defineProps<Props>()
+
+// const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const daysNumber = new Date(year, month, 0).getDate()
+const monthArray = Array.from({ length: daysNumber }, (_, i) => i + 1)
+</script>
+
+<style>
+.month {
+  display: grid;
+  height: 100%;
+  grid-template-columns: repeat(7, 1fr);
+  gap: 4px;
+}
+</style>
