5bbeedcfd9592056f75466069bd72c03330fe7c0
daramosn <daramosn@unal.edu.co>
2024-06-11 12:13:07 -0500

    chore: add CardModal component

---
+++ src/components/CardModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="isOpen" class="modal">
+    <div class="backdrop" @click="$emit('backdropClicked')"></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  isOpen: boolean
+}
+const { isOpen } = defineProps<Props>()
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: grid;
+  place-content: center;
+  z-index: 10;
+
+  .backdrop {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(1px);
+    background-color: rgb(0, 0, 0, 0.3);
+  }
+
+  & > *:nth-child(2) {
+    z-index: 11;
+  }
+}
+</style>
